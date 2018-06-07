f7c1249fdf9c370163ad9332e3aab72e47871acb
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2018-06-07 04:04:28 +0200

    Añadido servicio especial de parseo automatico de parametros.

--- Listeners/RequestParameterNameOverrideListener.php
+++ Listeners/RequestParameterNameOverrideListener.php
@@ -13,11 +13,80 @@ use Symfony\Component\HttpKernel\KernelEvents;
 
 class RequestParameterNameOverrideListener
 {
+    /**
+     * @var $request \Symfony\Component\HttpFoundation\Request
+     */
+    private $request;
+    private $queryParams;
+
     public function onKernelController(FilterControllerEvent $event)
     {
-        $request = $event->getRequest();
+        $this->request = $event->getRequest();
 
-        var_dump( $request->query->keys() );
-        die();
+        $this->clearQueryParams();
+        
+        $arrQueryParams = $this->getQueryParams();
+
+        foreach( $arrQueryParams as $queryKey => $queryValue ){
+
+            $this->request->query->set( $queryKey, $queryValue );
+
+        }
+    }
+
+    private function clearQueryParams(){
+
+        $keys = $this->request->query->keys();
+
+        foreach ( $keys as $key ){
+
+            $this->request->query->remove( $key );
+
+        }
+
+    }
+
+    private function getQueryParams(){
+
+        $fullUrl = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+
+        if( !empty( $_SERVER['REQUEST_URI'] ) ) {
+
+            if (strpos($fullUrl, ".") !== false) {
+
+                $parsedUrl = parse_url($fullUrl);
+
+                if (array_key_exists('query', $parsedUrl)) {
+
+                    $queryParams = $parsedUrl['query'];
+
+                    if (strpos($queryParams, "&") !== false) {
+
+                        $queryParamsArray = explode("&", $queryParams);
+
+                        foreach( $queryParamsArray as $queryParam ){
+
+                            $queryParamData = explode( "=", $queryParam );
+
+                            $this->queryParams[ $queryParamData[0] ] = rawurldecode( $queryParamData[1] );
+
+                        }
+
+                    } else {
+
+                        $queryParamData = explode( "=", $queryParams );
+
+                        $this->queryParams[ $queryParamData[0] ] = rawurldecode( $queryParamData[1] );
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return $this->queryParams;
+
     }
 }
